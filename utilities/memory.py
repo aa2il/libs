bf3e98dd13ad791a631e693ef2933f672f7bbc3b
aa2il <jattili@yahoo.com>
2024-10-15 14:29:59 -0700

    Updates

--- utilities/memory.py
+++ utilities/memory.py
@@ -50,4 +50,26 @@ class Memory_Monitor:
         if self.LOG:
             self.LOG.write('%8.1f,%8.1f\n' % (dt,mem))
             self.LOG.flush()        
-        
+
+
+
+# inner psutil function
+def process_memory():
+    process = psutil.Process(os.getpid())
+    mem_info = process.memory_info()
+    return mem_info.rss
+
+# decorator function
+def memory_profile(func):
+    def wrapper(*args, **kwargs):
+
+        mem_before = process_memory()
+        result = func(*args, **kwargs)
+        mem_after = process_memory()
+        print("{}:consumed memory: {:,}".format(
+            func.__name__,
+            mem_before, mem_after, mem_after - mem_before))
+
+        return result
+    return wrapper
+
